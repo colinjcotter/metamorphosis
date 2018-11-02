b195cd7d1206839555b40a4500b2ef153c071035
Colin Cotter <colin.cotter@imperial.ac.uk>
2018-11-02 13:21:28 +0000

    fiddling

--- inner.py
+++ inner.py
@@ -37,14 +37,15 @@ thetaS = theta('+')
 dthetaS = dtheta('+')
 
 dS0 = dS(degree=6)
+ds0 = ds(degree=6)
 
 eqn = (
     dz*z*dx + div(b*dz)*I*dx
     - jump(b*dz,n)*thetaS*dS - inner(b*dz,n)*I0*ds
-    +  dI*div(b*z)*dx
-    - jump(b*z,n)*dthetaS*dS
+    -  dI*div(b*z)*dx
+    + jump(b*z,n)*dthetaS*dS
     + sig*thetaS*dthetaS*dS0
-)        
+)
 
 bcs = [DirichletBC(W.sub(2), 0, (1,2))]
 
@@ -56,10 +57,9 @@ sparams = {'ksp_type':'gmres',
            'mat_type':'matfree',
            'pc_type':'python',
            'pc_python_type':'scpc.HybridSCPC',
-           'hybrid_sc_ksp_type':'gmres',
+           'hybrid_sc_ksp_type':'preonly',
            'hybrid_sc_ksp_monitor':True,
-           'hybrid_sc_pc_type':'ilu',
-           'hybrid_sc_pc_factor_mat_solver_type':'mumps'}
+           'hybrid_sc_pc_type':'lu'}
 zSolver = NonlinearVariationalSolver(zProb,
                                      solver_parameters=sparams)
 
