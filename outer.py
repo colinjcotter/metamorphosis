18017681b709f3e32fbafbc0a917d2ea6dbd15c0
Colin Cotter <colin.cotter@imperial.ac.uk>
2018-11-09 18:39:25 +0000

    outer settings/

--- outer.py
+++ outer.py
@@ -47,20 +47,18 @@ eqn = (
     +  dI*div(b*z)*dx
     + jump(b*z,n)*dthetaS*dS
 )
+eqn += sig*dtheta*theta*ds
 
 eqn1 = eqn + sig*eps*thetaS*dthetaS/(eps**2 + thetaS**2)**0.5*dS0
 eqn0 = eqn + sig*thetaS*dthetaS*dS
 
-bcs = [DirichletBC(W.sub(2), 0, (1,2))]
-
 sparams = {'ksp_type':'gmres',
-           'ksp_monitor':True,
            'ksp_converged_reason':True,
            'mat_type':'matfree',
            'pc_type':'python',
            'pc_python_type':'scpc.HybridSCPC',
            'hybrid_sc_ksp_type':'gmres',
-           'hybrid_sc_ksp_monitor':True,
+           'hybrid_sc_ksp_converged_reason':False,
            'hybrid_sc_pc_type':'lu'}
 
 sparams0 = {'ksp_type':'preonly',
@@ -69,29 +67,58 @@ sparams0 = {'ksp_type':'preonly',
             'mat_type':'aij',
             'pc_type':'lu'}
 
-z0Prob = NonlinearVariationalProblem(eqn0, w, bcs=bcs)
+z0Prob = NonlinearVariationalProblem(eqn0, w)
 z0Solver = NonlinearVariationalSolver(z0Prob,
                                       solver_parameters=sparams)
 
-zProb = NonlinearVariationalProblem(eqn1, w, bcs=bcs)
+zProb = NonlinearVariationalProblem(eqn1, w)
 zSolver = NonlinearVariationalSolver(zProb,
                                      solver_parameters=sparams)
 
-z0Solver.solve()
-zSolver.solve()
+du = TestFunction(U)
+v0 = Function(U)
+v1 = Function(U)
+v2 = Function(U)
 
-J = assemble(1./2*z*z*dx)
+alpha = Constant(1.0e-2)
+
+v0eqn = (du*v1 + alpha**2*inner(grad(du),grad(v1)) - du*v0)*dx
+v1eqn = (du*v2 + alpha**2*inner(grad(du),grad(v2)) - du*v1)*dx
+v2eqn = (du*u + alpha**2*inner(grad(du),grad(u)) - du*v2)*dx
+
+v0Prob = NonlinearVariationalProblem(v0eqn, v1)
+v1Prob = NonlinearVariationalProblem(v1eqn, v2)
+v2Prob = NonlinearVariationalProblem(v2eqn, u)
+
+luparams = {'ksp_type':'gmres',
+            'pc_type':'sor'}
+
+v0Solver = NonlinearVariationalSolver(v0Prob,
+                                        solver_parameters=luparams)
+v1Solver = NonlinearVariationalSolver(v1Prob,
+                                        solver_parameters=luparams)
+v2Solver = NonlinearVariationalSolver(v2Prob,
+                                        solver_parameters=luparams)                                        
+v0Solver.solve()
+v1Solver.solve()
+v2Solver.solve()
+z0Solver.solve()
+#zSolver.solve()
+
+cu = Constant(1.0e-2)
+
+J = assemble(1./2*z*z*dx + cu*1./2*v0*v0*dx)
 m = Control(u)
 Jhat = ReducedFunctional(J, m)
 
-g_opt = minimize(Jhat, options={"disp": True})
+g_opt = minimize(Jhat, options={"disp": True, 'gtol':1.0e-3})
 
 u.assign(g_opt)
 z0Solver.solve()
 zSolver.solve()
 
 z, I, T = w.split()
-f = File('IZ.pvd')
+f = File('IZ2.pvd')
 
 z0Solver.solve()
 f.write(I,z)
